4268d56e9f46ba92e9f03a2dc339c4e353fa0bc4
hp2025-final <hp2025.final@gmail.com>
2026-06-29 10:16:32 -0700

    Update robots.txt: allow AI crawlers (GPTBot, ClaudeBot, PerplexityBot, GoogleOther)

--- app/robots.ts
+++ app/robots.ts
@@ -4,15 +4,33 @@ export default function robots(): MetadataRoute.Robots {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.pcwalaonline.com";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: ["/", "/_next/static/"],
-      disallow: [
-        "/search",    // Search queries consume crawl budget and lead to index bloat
-        "/api/",      // Next.js API directory should not be crawled
-        "/_next/",    // Next.js build files (but static assets are allowed above)
-      ],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/search"],
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "GoogleOther",
+        allow: "/",
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
